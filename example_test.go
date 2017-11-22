04a56d7463826b5e944321c4850d2005c0211921
Joël Gähwiler <joel.gaehwiler@gmail.com>
2017-11-22 13:45:33 +0100

    added example

---
+++ example_test.go
@@ -0,0 +1,53 @@
+package mgojq
+
+import (
+	"fmt"
+	"time"
+
+	"gopkg.in/mgo.v2/bson"
+)
+
+func Example() {
+	// get jobs collection
+	coll := Wrap(db.C("jobs"))
+
+	// ensure indexes
+	err := coll.EnsureIndexes()
+	if err != nil {
+		panic(err)
+	}
+
+	// create a worker pool
+	pool := NewPool(1, 100*time.Millisecond, 1*time.Hour)
+
+	// register worker
+	pool.Register("Adder", func(c *Collection, j *Job, q <-chan struct{}) error {
+		r := j.Params["a"].(int) + j.Params["b"].(int)
+		c.Complete(j.ID, bson.M{"r": r})
+		return nil
+	})
+
+	// start pool
+	pool.Start(coll)
+	defer pool.Close()
+
+	// add job
+	id, err := coll.Enqueue("Adder", bson.M{"a": 10, "b": 5}, 0)
+	if err != nil {
+		panic(err)
+	}
+
+	// wait some time
+	time.Sleep(200 * time.Millisecond)
+
+	// get job
+	job, err := coll.Fetch(id)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%+v", job.Result["r"])
+
+	// Output:
+	// 15
+}
